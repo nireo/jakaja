dadd5d3c3851ef2c62334bc342c5814283fc4739
nireo <eemil.lehtikangas1@gmail.com>
2022-09-03 12:00:28 +0300

    added a httputil file and updated the README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # jakaja
+
+A minimal, fast and robust distributed file system using nginx as storage servers and a single master to control file distribution, replication and management.
+
+## Usage
+
+```
+# Starting storage servers
+$ PORT=9001 ./storage
+...
+
+$ ./jakaja --storages=https://localhost:9001,https://localhost:9002,https://localhost:9003 --action=serve --db=index.db
+```
+
+## Benchmarks
+
+TODO
---
+++ engine/util.go
@@ -0,0 +1,81 @@
+package engine
+
+// util.go contains utility functions to make making http requests easier.
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func httpget(addr string) ([]byte, error) {
+	resp, err := http.Get(addr)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("httpget: got status %d", resp.StatusCode)
+	}
+	return ioutil.ReadAll(resp.Body)
+}
+
+func httpdel(addr string) error {
+	req, err := http.NewRequest(http.MethodDelete, addr, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent &&
+		resp.StatusCode != http.StatusNotFound {
+		return fmt.Errorf("httpdel: status code is not 204 or 404; got: %d", resp.StatusCode)
+	}
+	return nil
+}
+
+func httpput(addr string, body io.Reader, clen int64) error {
+	req, err := http.NewRequest(http.MethodPut, addr, body)
+	if err != nil {
+		return err
+	}
+	req.ContentLength = clen
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent &&
+		resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("httpput: status code is not 201 or 204; got: %d", resp.StatusCode)
+	}
+	return nil
+}
+
+func httpheader(addr string, timeout time.Duration) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodHead, addr, nil)
+	if err != nil {
+		return false, err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode == http.StatusOK, nil
+}
